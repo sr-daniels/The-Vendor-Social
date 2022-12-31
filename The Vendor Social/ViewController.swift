f59981f39ed695ba175ccf80da4cca9ce97bec0c
Sharonda Daniels <sharonda.d27@gmail.com>
2022-12-31 18:36:19 -0500

    Initial Commit

---
+++ The Vendor Social/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Vendor Social
+//
+//  Created by Sharonda Daniels on 12/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
